b77b7a05f8d300a51525d0634b6647534250de5d
Akash Rawat <3150akash@gmail.com>
2020-04-11 15:30:39 +0000

    added method testing

--- src/app.test.js
+++ src/app.test.js
@@ -33,4 +33,16 @@ describe("App component", () => {
         const component = findByAttr(wrapper, "appComponent");
         expect(component.length).toBe(1)
     })
+    it("example method to update state must work expectedly", () => {
+        const classInstance = wrapper.instance()
+        classInstance.exampleMethod_updatesState();
+        const newState = classInstance.state.hideBtn
+        expect(newState).toBe(true);
+    })
+    it("example method to return must work expectedly", () => {
+        const classInstance = wrapper.instance()
+        const newVal = classInstance.exampleMethod_returnsAValue(6);
+        // const newState= classInstance.state.hideBtn
+        expect(newVal).toBe(7);
+    })
 })
